41fe8b70e4990d0c95f336c1e7cbc86c39e2c025
Arthur Dantas <absd2@cin.ufpe.br>
2020-01-30 01:01:32 -0300

    Improves answer and creates log file.

--- a.c
+++ a.c
@@ -4,6 +4,8 @@
 #define ll long long
 
 void runFromFile(char *filename) {
+    ll int i, j;
+
     // Loads input file.
     FILE *fRead = fopen(filename, "rt");
     if(!fRead) {
@@ -12,7 +14,6 @@ void runFromFile(char *filename) {
     }
 
     // Reads input from file.
-    ll int i;
     ll int maxSlices, nPizzas;
     
     fscanf(fRead, "%lld %lld", &maxSlices, &nPizzas);
@@ -29,29 +30,65 @@ void runFromFile(char *filename) {
         exit(-1);
     }
 
+    // Loads log file.
+    FILE *fLog = fopen("log/log.out", "wt");
+    if(!fLog) {
+        printf("Could not create log file.\n");
+        exit(-1);
+    }
+
     // Processes the answer.
-    ll int *indexes=NULL;
-    ll int sum=0, nAnswers=0;
-    for(i=nPizzas-1; i>=0; i--) {
-        if(slices[i]+sum<maxSlices) {
-            sum+=slices[i];
-            
-            nAnswers++;
-            indexes=(ll int *)realloc(indexes, sizeof(ll int)*nAnswers);
-            indexes[nAnswers-1]=i;
+    ll int *currentIndexes=NULL, *maxIndexes=NULL;
+    ll int currentSum=0, currentAnswers=0, start=nPizzas-1, maxSum=0, maxAnswers=0;
+    while(start>1) {
+        // Looks for an answers by searching from the very last element to the back, and moves the beggining point one step to the left every iteration.
+        for(i=start; i>=0; i--) {
+            if(slices[i]+currentSum<maxSlices) {
+                currentSum+=slices[i];
+                
+                currentAnswers++;
+                currentIndexes=(ll int *)realloc(currentIndexes, sizeof(ll int)*currentAnswers);
+                currentIndexes[currentAnswers-1]=i;
+            }
         }
+
+        fprintf(fLog, "cSum=%lld | maxSum=%lld\n", currentSum, maxSum);
+
+        // If the answer now found is bigger than the maximum answer, replaces it.
+        if(currentSum>maxSum) {
+            maxSum=currentSum;
+            maxAnswers=currentAnswers;
+            maxIndexes=(ll int *)realloc(maxIndexes, sizeof(ll int)*currentAnswers);
+
+            for(i=0; i<currentAnswers; i++) {
+                maxIndexes[i]=currentIndexes[i];
+            }
+        }
+
+        currentSum=0;
+        currentAnswers=0;
+        currentIndexes=NULL;
+        free(currentIndexes);
+
+        start--;
     }
 
     // Shows answer.
-    fprintf(fWrite, "%lld\n", nAnswers);
-    for(i=0; i<nAnswers; i++) {
-        fprintf(fWrite, "%lld ", indexes[i]);
+    fprintf(fWrite, "%lld\n", maxAnswers);
+    for(i=0; i<maxAnswers; i++) {
+        if(i==maxAnswers) {
+            fprintf(fWrite, "%lld", maxIndexes[i]);
+        }else{
+            fprintf(fWrite, "%lld ", maxIndexes[i]);
+        }
     }
 
 
     fclose(fRead);
     fclose(fWrite);
     free(slices);
+    free(currentIndexes);
+    free(maxIndexes);
 }
 
 int main() {
